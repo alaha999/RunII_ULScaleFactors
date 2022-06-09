df89a4fae12b5dd514e8f127ab0a9df7ce641a56
ykumar05 <kumar.yash@students.iiserpune.ac.in>
2022-06-09 14:42:16 +0530

    first commit

---
+++ 2016/post_VFP/Taus/tau_sf.py
@@ -0,0 +1,24 @@
+from correctionlib import _core
+
+# Load CorrectionSet
+fname = '/home/bowerbird/Yash/Work/Jupytr/Scale_Factors/2016/post_VFP/Taus/tau_UL2016_postVFP_new.json'
+cset = _core.CorrectionSet.from_file(fname)
+
+# Load Correction objects that can be evaluated
+corr1 = cset["DeepTau2017v2p1VSjet"]
+corr2 = cset["DeepTau2017v2p1VSe"]
+corr3 = cset["DeepTau2017v2p1VSmu"]
+
+pt, eta_e,eta_mu, dm, genmatch =  1510.0, 2.1, 2.1, 0, 5
+
+# DeepTau2017v2p1VSjet
+sf1 = corr1.evaluate(pt,dm,5,"VTight","nom","pt")
+print("DeepTau2017v2p1VSjet sf= %.8f (genmatch=5)"%sf1)
+
+# DeepTau2017v2p1VSe
+sf2 = corr2.evaluate(eta_e,1,"Loose","nom")
+print("DeepTau2017v2p1VSe sf= %.5f (genmatch=1)"%sf2)
+
+# DeepTau2017v2p1VSmu
+sf3 = corr3.evaluate(eta_mu,2,"Loose","nom")
+print("DeepTau2017v2p1VSmu sf= %.5f (genmatch=2)"%sf3)
